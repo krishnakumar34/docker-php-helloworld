f2089c68469306153fc944dbba40133d496923c7
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2022-09-30 00:51:34 +0530

    Add files via upload
    Updated

---
+++ src/einthu.php
@@ -0,0 +1,10 @@
+<?php
+$cid=$_GET['c']; 
+//$ch=$_GET['ch']; 
+//$p = $_GET['p']; 
+$url="https://ettmv.onrender.com/einthu/$cid";
+$m3u=file_get_contents($url);$m3uk="https://corsnow3.onrender.com/$m3u";
+//echo var_dump($haystack1);
+header("Location:$m3uk");
+?>;
+

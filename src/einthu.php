5b00cc23aecfac2cafde76fceb2b35231c81c433
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2022-09-30 01:00:16 +0530

    Update einthu.php
    Updated

--- src/einthu.php
+++ src/einthu.php
@@ -1,9 +1,15 @@
-<?php
-$cid=$_GET['c']; 
-//$ch=$_GET['ch']; 
+<?php
+
+$cid=$_GET['c']; 
+
+//$ch=$_GET['ch']; 
+
 //$p = $_GET['p']; 
 $url="https://ettmv.onrender.com/einthu/$cid";
-$m3u=file_get_contents($url);$m3uk="https://corsnow3.onrender.com/$m3u";
+
+$m3u=file_get_contents($url);
+
+$m3uk="https://corsnow3.onrender.com/$m3u";
 //echo var_dump($haystack1);
 header("Location:$m3uk");
 ?>;

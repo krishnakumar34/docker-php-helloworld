6eb5f6d7d418958149b4ce7d9589c76d3377980a
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2022-10-14 20:53:56 +0530

    Update ytube.php
    Updated

--- src/ytube.php
+++ src/ytube.php
@@ -5,7 +5,7 @@ $cid=$_GET['c'];
 //$ch=$_GET['ch']; 
 
 //$p = $_GET['p']; 
-$url="https://kk.ramkumarmj.repl.co/hello/$cid";
+$url="https://ettmv.onrender.com/hello/$cid";
 $m3u=file_get_contents($url);
 
 //echo var_dump($haystack1);

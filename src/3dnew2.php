bbe3e508e351c5e7b836cfdb247d663879b8ee7e
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2022-11-13 05:53:32 +0530

    Add files via upload
    Updated

---
+++ src/3dnew2.php
@@ -0,0 +1,103 @@
+<?php
+$cid=$_GET['c']; 
+//$ch=$_GET['ch']; 
+//$p = $_GET['p']; 
+$opts = [
+    "http" => [
+        "method" => "GET",
+        "header" =>  "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
+"X-User-Agent: Model: MAG250; Link: WiFi\r\n".
+"Referer: http://3d.new4k.tv/stalker_portal/c/\r\n" .
+"Cookie:mac=00:1A:79:a1:1d:20;stb_lang=en; timezone=GMT\r\n" .
+"Accept: */*\r\n".
+"Host: 3d.new4k.tv\r\n"
+    ]
+];
+	
+$context = stream_context_create($opts);
+$urltk="http://3d.new4k.tv:80/stalker_portal/server/load.php?type=stb&action=handshake&token=&JsHttpRequest=1-xml";
+$haystack = file_get_contents($urltk,false,$context);
+//echo $haystack;
+$json = json_decode($haystack, true);
+$id='4548';
+//echo var_dump($json);
+$token=$json['js']['token'];
+//echo $token;
+$opts1 = [
+    "http" => [
+        "method" => "GET",
+        "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
+"X-User-Agent: Model: MAG250; Link: WiFi\r\n".
+"Referer: http://3d.new4k.tv/stalker_portal/c/\r\n" .
+"Cookie:mac=00:1A:79:a1:1d:20;stb_lang=en; timezone=GMT\r\n" .
+"Authorization:Bearer $token\r\n".
+"Accept: */*\r\n".
+"Host: 3d.new4k.tv\r\n"
+    ]
+];
+$opts2 = [
+    "http" => [
+        "method" => "GET",
+        "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
+"X-User-Agent: Model: MAG250; Link: WiFi\r\n".
+"Referer: http://3d.new4k.tv/stalker_portal/c/\r\n" .
+"Cookie:mac=00:1A:79:a1:1d:20;stb_lang=en;timezone=GMT\r\n" .
+"Authorization:Bearer $token\r\n".
+"Accept: */*\r\n".
+"Host: 3d.new4k.tv\r\n".
+"Connection:Keep-Alive\r\n"
+    ]
+];
+$s="_";
+$t=time();
+$urpf="http://3d.new4k.tv:80/stalker_portal/server/load.php?type=stb&action=get_profile&hd=1&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.1.0%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566&num_banks=2&sn=E7C35E8DD26F1&stb_type=MAG250&image_version=218&video_out=hdmi&device_id=&device_id2=&signature=&auth_second_step=1&hw_version=1.7-BD-00&not_valid_token=0&client_type=STB&hw_version_2=e7c35e8dd26f1fc0b409b291f1a2af0a&timestamp=$t&api_signature=263&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3AA1%3A1D%3A20%22%2C%22sn%22%3A%22E7C35E8DD26F1%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22566883ca805996f9d5ba91692301480a%22%7D&JsHttpRequest=1-xml";
+//$urlpf="http://3d.new4k.tv:80/stalker_portal/server/load.php?type=stb&action=get_profile&hd=1&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.1.0%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566&num_banks=2&sn=E7C35E8DD26F1&stb_type=MAG250&image_version=218&video_out=hdmi&device_id=&device_id2=&signature=&auth_second_step=1&hw_version=1.7-BD-00&not_valid_token=0&client_type=STB&hw_version_2=e7c35e8dd26f1fc0b409b291f1a2af0a&timestamp=$t&api_signature=263&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3AA1%3A1D%3A20%22%2C%22sn%22%3A%22E7C35E8DD26F1%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%2209c571a00fe8a1f999a6e9d98655f024%22%7D&JsHttpRequest=1-xml";
+$pf2="http://3d.new4k.tv:80/stalker_portal/server/load.php?type=stb&action=get_profile&JsHttpRequest=1-xml&hd=1&num_banks=2&not_valid_token=0&auth_second_step=0&timestamp=$t&video_out=hdmi&client_type=STB&metrics=%7B%22mac%22%3A%2200%3A1a%3A79%3Aa1%3A1d%3A20%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG254%22%2C%22type%22%3A%22stb%22%2C%22uid%22%3A%22%22%7D";
+//$urlpf="http://starshare.live:8080/server/load.php?&action=get_profile&mac=00%3A1A%3A79%3A29%3A28%3AB4&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&hw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A29%3A28%3AB4%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22null%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
+//$urlpf="http://starshare.live:8080/server/load.php?&action=get_profile&mac=00%3A1A%3A79%3A29%3A28%3AB4&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&hw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A29%3A28%3AB4%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22null%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
+//$urlpf="http://sunset.pointto.us/stalker_portal/server/load.php?&action=get_profile&random=efd90c006734d9a2e0d9e74cec8ac03605675087&mac=00%3A1A%3A79%3AE5%3AB7%3A2D&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&hw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3AE5%3AB7%3A2D%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22efd90c006734d9a2e0d9e74cec8ac03605675087%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
+//echo $urlch;
+
+//if(!file_exists($cache)){
+////
+$context1 = stream_context_create($opts1);
+$haystack1=file_get_contents($pf2,false,$context1);
+//echo var_dump($haystack1);
+
+
+$s="_";
+$json1 = json_decode($haystack1, true);
+/*$context3 = stream_context_create($opts1);
+
+$haystack3=file_get_contents($pf2,false,$context3);
+
+//echo var_dump($haystack3);
+
+$json3= json_decode($haystack3, true);*/
+$urlch="http://3d.new4k.tv:80/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http%3A%2F%2Flocalhost%2Fch%2F$cid&JsHttpRequest=1-xml";
+//echo $urlch;
+//$urlch="http://starshare.live:8080/portal.php?type=itv&action=create_link&cmd=ffmpeg%20http%3A%2F%2Flocalhost%2Fch%2F$cid_";
+//$urlch="http://sunset.pointto.us/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http%3A%2F%2Flocalhost%2Fch%2F$cid";
+//$urlch="http://tv.indigotv.me:80/stalker_portal/server/load.php?type=stb&action=get_profile&hd=1&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.1.0%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566&num_banks=2&sn=23649D9B04350&stb_type=MAG250&image_version=218&video_out=hdmi&device_id=&device_id2=&signature=&auth_second_step=1&hw_version=1.7-BD-00&not_valid_token=0&client_type=STB&hw_version_2=23649d9b04350f116e306a0b598a798a&timestamp=$t&api_signature=263&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A00%3A11%3A9C%22%2C%22sn%22%3A%2223649D9B04350%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22b8c4a85457c1cf44af6dc5ab06b64a27%22%7D&JsHttpRequest=1-xml";
+//echo $urlch;
+
+//if(!file_exists($cache)){
+////
+$context2 = stream_context_create($opts2);
+$haystack2=file_get_contents($urlch,false,$context1);
+//echo var_dump($haystack2);
+$json2 = json_decode($haystack2, true);
+$id='4548';
+$data=$json2['js']['cmd'];
+//echo $data;
+//$data=$data | "User-Agent=Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3";
+//$m3u=$json['js']['data'][$ch]['cmd'];
+//echo "values".var_dump($m3u);
+//$m3u=$data[0];
+//echo "m3ulink".var_dump($m3u);
+//$data1=preg_replace( '/\s+/', '', $data );
+//echo $data1;
+//$m3unew=str_replace('ffmpeg', '',$data1);
+//echo "value1" .$m3unew;
+header("Location:$data");
+?>;

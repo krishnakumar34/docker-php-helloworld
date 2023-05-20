e7ea7b56f902b34d088194968656f1eaf42f5403
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2023-05-20 10:44:55 +0530

    Create yogi.php

---
+++ src/yogi.php
@@ -0,0 +1,59 @@
+<?php
+
+$cid=$_GET['c']; 
+
+//$p = $_GET['p']; 
+
+$url1="https://vembx.one/$cid.html";
+
+//echo $url1;
+
+//call api
+
+//$jsonvalues = file_get_contents($url1);
+
+//echo "results1" .$jsonvalues;
+
+//echo "objecttype".gettype($jsonvalues);
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url1);
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,FALSE);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array( "Content-Type: application/json" ));
+
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Linux; Android 11; motorola one fusion+ Build/RPIS31.Q2-42-25-3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/94.0.4606.85 Mobile Safari/537.36");
+
+$response = curl_exec($ch);
+
+//echo "result".var_dump($response);
+
+$json = json_encode($response, true);
+
+//echo "result3".var_dump($json);
+
+preg_match("/file:(.*)\m3u8/", $json,$results) ;
+
+//echo "result4".var_dump($results[0]);
+
+$data=$results[0];
+
+$m3unew=str_replace('\\' , '',$results[0]);
+
+$m3u=str_replace('file:' , '',$m3unew);
+
+$m3u= trim($m3u, '"');
+
+echo file_get_contents($m3u);
+
+//header("Location: $m3u");
+
+//die();
+
+?>

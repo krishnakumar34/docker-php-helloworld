5cda3eb11e8bff2e9999fc5b21b97f27f86b1670
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2023-08-22 17:29:17 +0530

    Add files via upload

--- src/epic7k24.php
+++ src/epic7k24.php
@@ -5,11 +5,10 @@ $cid=$_GET['c'];
 $opts = [
     "http" => [
         "method" => "GET",
-        "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
+       "header" => "Cookie: mac=00:1a:79:00:00:88\r\n".
+"User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
 "X-User-Agent: Model: MAG250; Link: WiFi\r\n".
-"Referer: http://epic7k.com/stalker_portal/c/\r\n" .
-"Cookie:mac=00:1a:79:32:fe:6e; stb_lang=en; timezone=GMT\r\n" .
-"Accept: */*\r\n".
+"Authorization:Bearer $token\r\n" .
 "Host: epic7k.com\r\n"
     ]
 ];
@@ -26,30 +25,28 @@ $token=$json['js']['token'];
 $opts1 = [
     "http" => [
         "method" => "GET",
-        "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
+        "header" => "Cookie: mac=00:1a:79:00:00:88\r\n".
+"User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
 "X-User-Agent: Model: MAG250; Link: WiFi\r\n".
-"Referer: http://epic7k.com/stalker_portal/c/\r\n" .
-"Cookie:mac=00:1a:79:32:fe:6e; stb_lang=en; timezone=GMT\r\n" .
-"Accept: */*\r\n".
-"Authorization:Bearer $token\r\n".
+"Authorization:Bearer $token\r\n" .
 "Host: epic7k.com\r\n"
     ]
 ];
 $opts2 = [
     "http" => [
         "method" => "GET",
-        "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
+        "header" => "Cookie: mac=00:1a:79:00:00:88\r\n".
+"User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
 "X-User-Agent: Model: MAG250; Link: WiFi\r\n".
-"Referer: http://epic7k.com/stalker_portal/c/\r\n" .
-"Cookie:mac=00:1a:79:32:fe:6e; stb_lang=en; timezone=GMT\r\n" .
-"Accept: */*\r\n".
-"Authorization:Bearer $token\r\n".
+"Authorization:Bearer $token\r\n" .
 "Host: epic7k.com\r\n"
     ]
 ];
+
 $s="_";
 $t=time();
-$urlpf="http://epic7k.com:80/stalker_portal/server/load.php?&action=get_profile&random=199b04c5a44ec9ccc2fffe5fc2cdd3b9de1ffc06&mac=00%3A1a%3A79%3A00%3A00%3A88&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&hw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1a%3A79%3A00%3A00%3A88%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22199b04c5a44ec9ccc2fffe5fc2cdd3b9de1ffc06%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
+$urlpf="http://epic7k.com:80/stalker_portal/server/load.php?type=stb&action=get_profile&device_id=&device_id2=&auth_second_step=1&metrics=%7B%22mac%22%3A%2200%3A1a%3A79%3A00%3A00%3A88%22%7D&hw_version_2=d6b3eba9bdc7fb3858d07cdcaa7a27ed8b10ecc6";
+//$urlpf="http://epic7k.com:80/stalker_portal/server/load.php?&action=get_profile&random=199b04c5a44ec9ccc2fffe5fc2cdd3b9de1ffc06&mac=00%3A1a%3A79%3A00%3A00%3A88&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&timestamp=$&thw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1a%3A79%3A00%3A00%3A88%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22199b04c5a44ec9ccc2fffe5fc2cdd3b9de1ffc06%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
 //$urlpf="http://epic7k.com:80/stalker_portal/server/load.php?type=stb&action=get_profile&hd=1&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.1.0%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566&num_banks=2&sn=1AAA2EF393BED&stb_type=MAG250&image_version=218&video_out=hdmi&device_id=&device_id2=&signature=&auth_second_step=1&hw_version=1.7-BD-00&not_valid_token=0&client_type=STB&hw_version_2=1aaa2ef393bed90827c8d93d11bda392&timestamp=$t&api_signature=263&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A00%3A00%3A88%22%2C%22sn%22%3A%221AAA2EF393BED%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%2225b7477574eeec87486783efbd490c41%22%7D&JsHttpRequest=1-xml";
 //$urlpf="http://epic7k.com:80/stalker_portal/server/load.php?type=stb&action=get_profile&hd=1&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.1.0%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566&num_banks=2&sn=E125C77287C0B&stb_type=MAG250&image_version=218&video_out=hdmi&device_id=&device_id2=&signature=&auth_second_step=1&hw_version=1.7-BD-00&not_valid_token=0&client_type=STB&hw_version_2=e125c77287c0b7b072ed2a734545b268&timestamp=$t&api_signature=263&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A32%3AFE%3A6E%22%2C%22sn%22%3A%22E125C77287C0B%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22b8e63fb7dfd0ec4ee9a2ec188d76769f%22%7D&JsHttpRequest=1-xml";
 //echo $urlch;
@@ -58,7 +55,7 @@ $urlpf="http://epic7k.com:80/stalker_portal/server/load.php?&action=get_profile&
 ////
 $context1 = stream_context_create($opts1);
 $haystack1=file_get_contents($urlpf,false,$context1);
-echo var_dump($haystack1);
+//echo var_dump($haystack1);
 $json1 = json_decode($haystack1, true);
 
 
@@ -70,11 +67,11 @@ $urlch="http://epic7k.com:80/stalker_portal/server/load.php?type=itv&action=crea
 ////
 $context2 = stream_context_create($opts2);
 $haystack2=file_get_contents($urlch,false,$context1);
-echo var_dump($haystack2);
+//echo var_dump($haystack2);
 $json2 = json_decode($haystack2, true);
 $id='4548';
 $data=$json2['js']['cmd'];
-echo $data;
+//echo $data;
 #$data=$data | "User-Agent=Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3";
 //$m3u=$json['js']['data'][$ch]['cmd'];
 //echo "values".var_dump($m3u);

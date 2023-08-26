d766f7b7e471135382e53270d6512d0f11c11c5f
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2023-08-26 14:50:39 +0530

    Add files via upload

--- src/push4kvod.php
+++ src/push4kvod.php
@@ -8,7 +8,7 @@ $opts = [
         "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
 "X-User-Agent: Model: MAG250; Link: WiFi\r\n".
 "Referer: http://watch.push4k.tv/stalker_portal/c/\r\n" .
-"Cookie: mac=00:1A:79:E9:79:62; stb_lang=en; timezone=GMT\r\n" .
+"Cookie: mac=00:1A:79:5c:7e:2e; stb_lang=en; timezone=GMT\r\n" .
 "Accept: */*\r\n".
 "Host: watch.push4k.tv\r\n"
     ]
@@ -29,7 +29,7 @@ $opts1 = [
         "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
 "X-User-Agent: Model: MAG250; Link: WiFi\r\n".
 "Referer: http://watch.push4k.tv/stalker_portal/c/\r\n" .
-"Cookie:mac=00:1A:79:E9:79:62; stb_lang=en; timezone=GMT\r\n" .
+"Cookie:mac=00:1A:79:5c:7e:2e; stb_lang=en; timezone=GMT\r\n" .
 "Accept: */*\r\n".
 "Authorization:Bearer $token\r\n".
 "Host: watch.push4k.tv\r\n"
@@ -41,7 +41,7 @@ $opts2 = [
         "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
 "X-User-Agent: Model: MAG250; Link: WiFi\r\n".
 "Referer: http://watch.push4k.tv/stalker_portal/c/\r\n" .
-"Cookie:mac=00:1A:79:E9:79:62; stb_lang=en; timezone=GMT\r\n" .
+"Cookie:mac=00:1A:79:5c:7e:2e; stb_lang=en; timezone=GMT\r\n" .
 "Accept: */*\r\n".
 "Authorization:Bearer $token\r\n".
 "Host: watch.push4k.tv\r\n"
@@ -49,7 +49,8 @@ $opts2 = [
 ];
 $s="_";
 $t=time();
-$urlpf="http://watch.push4k.tv:80/stalker_portal/server/load.php?&action=get_profile&random=39b5023ee6786d443e81418f195126ae8238565a&mac=00%3A1A%3A79%3A13%3ABB%3A73&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&hw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3AE9%3A79%3A62%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%2239b5023ee6786d443e81418f195126ae8238565a%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
+$urlpf="http://watch.push4k.tv:80/stalker_portal/server/load.php?&action=get_profile&random=4cff4de3f6018cb84a54295fe149b6c498b0567b&mac=00%3A1A%3A79%3A5c%3A7e%3A2e&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&hw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A5c%3A7e%3A2e%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%224cff4de3f6018cb84a54295fe149b6c498b0567b%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
+//$urlpf="http://watch.push4k.tv:80/stalker_portal/server/load.php?&action=get_profile&random=39b5023ee6786d443e81418f195126ae8238565a&mac=00%3A1A%3A79%3A13%3ABB%3A73&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&hw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3AE9%3A79%3A62%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%2239b5023ee6786d443e81418f195126ae8238565a%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
 //$urlpf="http://watch.push4k.tv:80/stalker_portal/server/load.php?type=stb&action=get_profile&hd=1&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.1.0%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566&num_banks=2&sn=FBE3A12938F68&stb_type=MAG250&image_version=218&video_out=hdmi&device_id=&device_id2=&signature=&auth_second_step=1&hw_version=1.7-BD-00&not_valid_token=0&client_type=STB&hw_version_2=fbe3a12938f6891ea6b3d69075c0cb1d&timestamp=$t&api_signature=263&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A6C%3A1F%3A2A%22%2C%22sn%22%3A%22FBE3A12938F68%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22222e0feab427fb511c15b7b793384c20%22%7D&JsHttpRequest=1-xml";
 //$urlpf="http://watch.push4k.tv:80/stalker_portal/server/load.php?type=stb&action=get_profile&hd=1&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.1.0%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566&num_banks=2&sn=1CEAFA6E767FC&stb_type=MAG250&image_version=218&video_out=hdmi&device_id=&device_id2=&signature=&auth_second_step=1&hw_version=1.7-BD-00&not_valid_token=0&client_type=STB&hw_version_2=1ceafa6e767fc866f57169f93630a107&timestamp=$t&api_signature=263&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A47%3A69%3A85%22%2C%22sn%22%3A%221CEAFA6E767FC%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random8b95b22666336e10a0fef55d6d0668f787e25ee3&JsHttpRequest=1-xml";
 //echo $urlch;

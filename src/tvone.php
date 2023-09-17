e01a9e9a55e9c2a72c8ce8d28dd0d66fc95e981b
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2023-09-17 08:42:14 +0530

    Add files via upload

--- src/tvone.php
+++ src/tvone.php
@@ -9,7 +9,34 @@ $p = $_GET['p'];
 		"X-User-Agent: Model: MAG250; Link: WiFi\r\n" .
 "Referer: http://line.tivi-one.net:80/c/\r\n" .
 "Cookie: mac=00:1A:79:4D:00:EF;stb_lang=en; timezone=GMT\r\n" .
-"Authorization:Bearer:0E2BDB36A2BA4EAF70F21F23AACAB942" .
+"Accept: */*\r\n"
+
+    ]
+];
+
+$context = stream_context_create($opts);
+
+$urltk="http://line.tivi-one.net:80/server/load.php?type=stb&action=handshake&token=&JsHttpRequest=1-xml";
+
+$haystack = file_get_contents($urltk,false,$context);
+
+//echo $haystack;
+
+$json = json_decode($haystack, true);
+
+$id='4548';
+
+//echo var_dump($json);
+
+$token=$json['js']['token'];
+$opts1 = [
+    "http" => [
+        "method" => "GET",
+        "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
+		"X-User-Agent: Model: MAG250; Link: WiFi\r\n" .
+"Referer: http://line.tivi-one.net:80/c/\r\n" .
+"Cookie: mac=00:1A:79:4D:00:EF;stb_lang=en; timezone=GMT\r\n" .
+"Authorization:Bearer:$token" .
 "Accept: */*\r\n"
 
     ]
@@ -18,9 +45,10 @@ $p = $_GET['p'];
 
 //$cache=str_replace("/","_",$_REQUEST["key"]);
 
+
 //if(!file_exists($cache)){
 
-$context = stream_context_create($opts);
+$context = stream_context_create($opts1);
 $haystack = file_get_contents("http://line.tivi-one.net:80/server/load.php?type=itv&action=get_ordered_list&genre=$cid&force_ch_link_check=&p=$p&JsHttpRequest=1-xml/",false,$context);
 //echo $haystack;
 $json = json_decode($haystack, true);

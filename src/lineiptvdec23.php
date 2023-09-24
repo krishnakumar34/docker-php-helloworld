feb4b8f002a82d7b3fb63138a4901cac9d0d02cc
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2023-09-24 17:59:17 +0530

    Add files via upload

--- src/lineiptvdec23.php
+++ src/lineiptvdec23.php
@@ -2,7 +2,33 @@
 $cid=$_GET['c']; 
 $ch=$_GET['ch']; 
 $p = $_GET['p']; 
-	$opts = [
+$opts = [
+    "http" => [
+        "method" => "GET",
+        "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
+		"X-User-Agent: Model: MAG250; Link: WiFi\r\n" .
+"Cache-Control: no-cache\r\n".
+"Referrer: http://line.crystal-ott.com/c/\r\n" .
+"Cookie: mac=00:1A:79:1A:00:10;stb_lang=en; timezone=GMT\r\n" .
+"Accept: */*\r\n"
+    ]
+];
+$context = stream_context_create($opts);
+
+$urltk="http://line.crystal-ott.com:80/server/load.php?type=stb&action=handshake&token=&JsHttpRequest=1-xml";
+
+$haystack = file_get_contents($urltk,false,$context);
+
+//echo $haystack;
+
+$json = json_decode($haystack, true);
+
+$id='4548';
+
+//echo var_dump($json);
+
+$token=$json['js']['token'];
+	$opts2 = [
     "http" => [
         "method" => "GET",
         "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
@@ -10,7 +36,7 @@ $p = $_GET['p'];
 "Referer: http://line.crystal-ott.com:80/c/\r\n" .
 "Cookie: mac=00:1A:79:1A:00:10;stb_lang=en; timezone=GMT\r\n" .
 "Accept: */*\r\n".
-"Authorization:Bearer:090D73C116B9AC45CB0FB0DF99A7DBCF"
+"Authorization:Bearer:$token"
 
     ]
 ];
@@ -20,7 +46,7 @@ $p = $_GET['p'];
 
 //if(!file_exists($cache)){
 
-$context = stream_context_create($opts);
+$context = stream_context_create($opts2);
 $haystack = file_get_contents("http://line.crystal-ott.com:80/server/load.php?type=itv&action=get_ordered_list&genre=$cid&p=$p",false,$context);
 //echo $haystack;
 $json = json_decode($haystack, true);

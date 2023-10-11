b1e54b2979b85efe787b8158788952c920794e29
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2023-10-11 16:19:49 +0530

    Add files via upload

--- src/britishtv.php
+++ src/britishtv.php
@@ -11,17 +11,12 @@ $opts = [
 
 
 "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
-
+"Referrer: http://live.britishtv.org:8090/stalker_portal/c/\r\n" .
 "X-User-Agent: Model: MAG250; Link: WiFi\r\n".
-
-"Referer: http://live.britishtv.org:8090/stalker_portal/c/\r\n" .
-
-"Cookie: mac=00:1a:79:42:E1:95; stb_lang=en; timezone=GMT\r\n" .
-
-"Accept: */*\r\n".
-
-
+"Cache-Control: no-cache\r\n".
 "Host: live.britishtv.org:8090\r\n"
+"Cookie: mac=00:1a:79:42:E1:95; stb_lang=en; timezone=Europe%2FParis\r\n" 
+
 
     ]
 

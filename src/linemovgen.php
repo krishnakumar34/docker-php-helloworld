f89b166ef2b111f1bdaf689d0329f1bc2d6e2036
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2023-10-15 07:36:26 +0530

    Update linemovgen.php

--- src/linemovgen.php
+++ src/linemovgen.php
@@ -61,7 +61,7 @@ $img= "$data[$i]["screenshot_uri"]";
 $imgnew=$img$cm
 //$url="https://kriskk.herokuapp.com/sidermay23.php?c=$cid&ch=$i&p=$j";
 
-echo "#EXTINF:-1 tvg-logo='$imgnew'.$data[$i]["name"];
+echo "#EXTINF:-1 tvg-logo='$img'.$data[$i]["name"];
 
 
 

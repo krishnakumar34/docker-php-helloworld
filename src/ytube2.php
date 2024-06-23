e6610462a66393306687f4e95c57fccb66107fbd
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2024-06-23 07:24:36 +0530

    Create ytube2.php

---
+++ src/ytube2.php
@@ -0,0 +1,14 @@
+<?php
+
+$cid=$_GET['c']; 
+
+//$ch=$_GET['ch']; 
+
+//$p = $_GET['p']; 
+$url="https://einthupy.vercel.app/hello/$cid";
+//$url="https://ytvv.onrender.com/hello/$cid";
+$m3u=file_get_contents($url);
+
+//echo var_dump($haystack1);
+header("Location:$m3u");
+?>

ffcfdceb6eb2898aa4482eec9104f4e130b3af75
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2024-11-03 19:02:07 +0530

    Add files via upload

--- src/anonym.php
+++ src/anonym.php
@@ -8,10 +8,9 @@ $opts = [
         "header" =>  "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
 "X-User-Agent: Model: MAG250; Link: WiFi\r\n".
 "Referer: http://anonymoustv.club/c/\r\n" .
-"Cookie:mac=00:1A:79:6A:76:FC;stb_lang=en; timezone=Europe%2FParis\r\n" .
+"Cookie:mac=00:1A:79:7D:7B:21;stb_lang=en; timezone=Europe%2FParis\r\n" .
 "Accept: */*\r\n".
-"Host: anonymoustv.club:-1\r\n".
-"Connection: Keep-Alive\r\n"
+"Host: anonymoustv.club:-1\r\n"
     ]
 ];
 	
@@ -30,11 +29,10 @@ $opts1 = [
         "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
 "X-User-Agent: Model: MAG250; Link: WiFi\r\n".
 "Referer: http://anonymoustv.club/c/\r\n" .
-"Cookie:mac=00:1A:79:6A:76:FC;stb_lang=en; timezone=Europe%2FParis\r\n" .
+"Cookie:mac=00:1A:79:7D:7B:21;stb_lang=en; timezone=Europe%2FParis\r\n" .
 "Authorization:Bearer $token\r\n".
 "Accept: */*\r\n".
-"Host: anonymoustv.club:-1\r\n".
-"Connection: Keep-Alive\r\n"
+"Host: anonymoustv.club:-1\r\n"
     ]
 ];
 $opts2 = [
@@ -43,16 +41,15 @@ $opts2 = [
         "header" => "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
 "X-User-Agent: Model: MAG250; Link: WiFi\r\n".
 "Referer: http://anonymoustv.club/c/\r\n" .
-"Cookie:mac=00:1A:79:6A:76:FC;stb_lang=en;timezone=Europe%2FParis\r\n" .
+"Cookie:mac=00:1A:79:7D:7B:21;stb_lang=en;timezone=Europe%2FParis\r\n" .
 "Authorization:Bearer $token\r\n".
 "Accept: */*\r\n".
-"Host: anonymoustv.club:-1\r\n".
-"Connection: Keep-Alive\r\n"
+"Host: anonymoustv.club:-1\r\n"
     ]
 ];
 $s="_";
 $t=time();
-$urlpf="http://anonymoustv.club:80/server/load.php?&action=get_profile&mac=00%3A1A%3A79%3A6A%3A76%3AFC&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A6A%3A76%3AFC%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22null%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
+$urlpf="http://anonymoustv.club:80/server/load.php?&action=get_profile&mac=00%3A1A%3A79%3A7D%3A7B%3A21&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A6A%3A76%3AFC%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22null%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
 //$urlpf="http://starshare.live:8080/server/load.php?&action=get_profile&mac=00%3A1A%3A79%3A29%3A28%3AB4&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&hw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A29%3A28%3AB4%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22null%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
 //$urlpf="http://starshare.live:8080/server/load.php?&action=get_profile&mac=00%3A1A%3A79%3A29%3A28%3AB4&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&hw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A29%3A28%3AB4%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22null%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
 //$urlpf="http://sunset.pointto.us/stalker_portal/server/load.php?&action=get_profile&random=efd90c006734d9a2e0d9e74cec8ac03605675087&mac=00%3A1A%3A79%3AE5%3AB7%3A2D&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&hw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3AE5%3AB7%3A2D%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22efd90c006734d9a2e0d9e74cec8ac03605675087%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";

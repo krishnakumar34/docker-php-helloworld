48d3f31a1f9ad1db3c23f59e456eb4c93af3c84f
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2024-11-09 12:18:10 +0530

    Update epic24.php

--- src/epic24.php
+++ src/epic24.php
@@ -8,8 +8,7 @@ $opts = [
        "header" => "Cookie: mac=00:1a:79:00:00:88\r\n".
 "User-Agent: Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3\r\n" .
 "X-User-Agent: Model: MAG250; Link: WiFi\r\n".
-"Host: 20.epiciptv.com:88\r\n".
-"Connection: Keep-Alive"	
+"Host: 20.epiciptv.com:88\r\n"	
     ]
 ];
 	

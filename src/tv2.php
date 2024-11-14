2b0176d9c9b466d129386f36c4e2944d892b8c65
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2024-11-14 13:10:55 +0530

    Update tv2.php

--- src/tv2.php
+++ src/tv2.php
@@ -13,7 +13,7 @@ $opts = [
 ];
   
 $context = stream_context_create($opts);
-$urltk="https://tv.tvzon.tv/stalker_portal/server/load.php?type=stb&action=handshake&token=&JsHttpRequest=1-xml";
+$urltk="http://tv.tvzon.tv/stalker_portal/server/load.php?type=stb&action=handshake&token=&JsHttpRequest=1-xml";
 $haystack = file_get_contents($urltk,false,$context);
 //echo $haystack;
 $json = json_decode($haystack, true);
@@ -44,7 +44,7 @@ $opts2 = [
 
 $s="_";
 $t=time();
-$urlpf="https://tv.tvzon.tv/stalker_portal/server/load.php?type=stb&action=get_profile&device_id=3F20FC3090DC482ED8695B03B7F39F66CA94B11E8945BADB86AAFCEFA98A16A6&device_id2=3F20FC3090DC482ED8695B03B7F39F66CA94B11E8945BADB86AAFCEFA98A16A6&auth_second_step=1&metrics=%7B%22mac%22%3A%2200%3A1a%3A79%3A1B%3A93%3A99%22%7D&hw_version_2=d6b3eba9bdc7fb3858d07cdcaa7a27ed8b10ecc6&timestamp=$t";
+$urlpf="http://tv.tvzon.tv/stalker_portal/server/load.php?type=stb&action=get_profile&device_id=3F20FC3090DC482ED8695B03B7F39F66CA94B11E8945BADB86AAFCEFA98A16A6&device_id2=3F20FC3090DC482ED8695B03B7F39F66CA94B11E8945BADB86AAFCEFA98A16A6&auth_second_step=1&metrics=%7B%22mac%22%3A%2200%3A1a%3A79%3A1B%3A93%3A99%22%7D&hw_version_2=d6b3eba9bdc7fb3858d07cdcaa7a27ed8b10ecc6&timestamp=$t";
 //$urlpf="https://tv.tvzon.tv/stalker_portal/server/load.php?type=stb&action=get_profile&device_id=8080347A4DEABDC811297C08931129BAF1AA4801083B002613BEBF87282A4E5B&device_id2=8080347A4DEABDC811297C08931129BAF1AA4801083B002613BEBF87282A4E5B&auth_second_step=1&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A2E%3A6D%3A8A%22%7D&hw_version_2=d6b3eba9bdc7fb3858d07cdcaa7a27ed8b10ecc6&timestamp=$t";
 //$urlpf="https://tv.tvzon.tv/stalker_portal/server/load.php?type=stb&action=get_profile&device_id=&device_id2=&auth_second_step=1&metrics=%7B%22mac%22%3A%2200%3A1a%3A79%3A00%3A00%3A88%22%7D&hw_version_2=d6b3eba9bdc7fb3858d07cdcaa7a27ed8b10ecc6";
 //$urlpf="https://tv.tvzon.tv/stalker_portal/server/load.php?&action=get_profile&random=199b04c5a44ec9ccc2fffe5fc2cdd3b9de1ffc06&mac=00%3A1a%3A79%3A00%3A00%3A88&type=stb&hd=1&sn=&stb_type=MAG250&client_type=STB&image_version=218&device_id=&hw_version=1.7-BD-00&timestamp=$&thw_version_2=1.7-BD-00&auth_second_step=1&video_out=hdmi&num_banks=2&metrics=%7B%22mac%22%3A%2200%3A1a%3A79%3A00%3A00%3A88%22%2C%22sn%22%3A%22%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22199b04c5a44ec9ccc2fffe5fc2cdd3b9de1ffc06%22%7D&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.6.1%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566";
@@ -60,7 +60,7 @@ echo var_dump($haystack1);
 $json1 = json_decode($haystack1, true);
 
 
-$urlch="https://tv.tvzon.tv/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http%3A%2F%2Flocalhost%2Fch%2F$cid&JsHttpRequest=1-xml";
+$urlch="http://tv.tvzon.tv/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%20http%3A%2F%2Flocalhost%2Fch%2F$cid&JsHttpRequest=1-xml";
 //$urlch="http://tv.indigotv.me:80/stalker_portal/server/load.php?type=stb&action=get_profile&hd=1&ver=ImageDescription%3A%200.2.18-r14-pub-250%3B%20ImageDate%3A%20Fri%20Jan%2015%2015%3A20%3A44%20EET%202016%3B%20PORTAL%20version%3A%205.1.0%3B%20API%20Version%3A%20JS%20API%20version%3A%20328%3B%20STB%20API%20version%3A%20134%3B%20Player%20Engine%20version%3A%200x566&num_banks=2&sn=23649D9B04350&stb_type=MAG250&image_version=218&video_out=hdmi&device_id=&device_id2=&signature=&auth_second_step=1&hw_version=1.7-BD-00&not_valid_token=0&client_type=STB&hw_version_2=23649d9b04350f116e306a0b598a798a&timestamp=1654751966&api_signature=263&metrics=%7B%22mac%22%3A%2200%3A1A%3A79%3A00%3A11%3A9C%22%2C%22sn%22%3A%2223649D9B04350%22%2C%22model%22%3A%22MAG250%22%2C%22type%22%3A%22STB%22%2C%22uid%22%3A%22%22%2C%22random%22%3A%22b8c4a85457c1cf44af6dc5ab06b64a27%22%7D&JsHttpRequest=1-xml";
 //echo $urlch;
 
